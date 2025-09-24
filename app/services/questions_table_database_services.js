d21083c6f31d87be5111e78e2e2a256ed6523fca
Heitor S. Ferreira <heitor.ferreira231203@gmail.com>
2025-09-24 20:32:07 -0300

    Working on Mobile Quiz Game | 24.09.2025

---
+++ app/services/questions_table_database_services.js
@@ -0,0 +1,84 @@
+import * as SQLite from 'expo-sqlite';
+
+// Abre conexão com o banco
+export async function getDbConnection() {
+    const cx = await SQLite.openDatabaseAsync('dbQuizGame.db');
+    return cx;
+}
+
+// Cria tabela se não existir
+export async function createQuestionsTable() {
+    const query = `
+    CREATE TABLE IF NOT EXISTS tbQuestions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT NULL,
+        id_theme INTEGER NOT NULL,
+        FOREIGN KEY (id_theme) REFERENCES tbThemes(id) ON DELETE CASCADE
+    )
+  `;
+    const cx = await getDbConnection();
+    await cx.execAsync(query);
+    await cx.closeAsync();
+}
+
+// Retorna todos os registros
+export async function getAllQuestions() {
+    const cx = await getDbConnection();
+    const questions = await cx.getAllAsync('SELECT * FROM tbQuestions');
+    await cx.closeAsync();
+
+    return questions.map(question => ({
+        id: question.id,
+        description: question.description,
+        id_theme: question.id_theme
+    }));
+}
+
+// Adiciona um registro
+export async function addQuestion(question) {
+    const cx = await getDbConnection();
+    const query = `
+    INSERT INTO tbQuestions (description, id_theme)
+    VALUES (?, ?)
+  `;
+    const result = await cx.runAsync(query, [
+        question.description,
+        question.id_theme
+    ]);
+    await cx.closeAsync();
+    return result.changes === 1;
+}
+
+// Atualiza um registro existente
+export async function changeQuestion(question) {
+    const cx = await getDbConnection();
+    const query = `
+    UPDATE tbQuestions
+    SET description = ?, id_theme = ?
+    WHERE id = ?
+  `;
+    const result = await cx.runAsync(query, [
+        question.id,
+        question.description,
+        question.id_theme
+    ]);
+    await cx.closeAsync();
+    return result.changes === 1;
+}
+
+// Deleta um registro pelo código
+export async function deleteQuestion(id) {
+    const cx = await getDbConnection();
+    const query = 'DELETE FROM tbQuestions WHERE id = ?';
+    const result = await cx.runAsync(query, [id]);
+    await cx.closeAsync();
+    return result.changes === 1;
+}
+
+// Deleta todos os registros
+export async function deleteAllQuestions() {
+    const cx = await getDbConnection();
+    const query = 'DELETE FROM tbQuestions';
+    await cx.execAsync(query);
+    await cx.closeAsync();
+}
